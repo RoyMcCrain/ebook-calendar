a1177e04f25af3e56d0d98f8be414ed3beabef35
RoyMcCrain <me@rrrr.dev>
2025-04-11 23:47:21 +0900

    Update

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -2,8 +2,8 @@ import { format } from "date-fns";
 
 /**
  * 今日の日付を「yyyy-MM-dd」形式でフォーマットして返します
- * 
- * @returns {string} 「yyyy-MM-dd」形式でフォーマットされた今日の日付
+ *
+ * @returns 「yyyy-MM-dd」形式でフォーマットされた今日の日付
  */
 export const formatToday = () => {
 	const today = new Date();
@@ -12,9 +12,9 @@ export const formatToday = () => {
 
 /**
  * 指定した日付を「yyyy-MM-dd」形式でフォーマットして返します
- * 
- * @param {Date} date - フォーマットする日付オブジェクト
- * @returns {string} 「yyyy-MM-dd」形式でフォーマットされた日付
+ *
+ * @param date - フォーマットする日付オブジェクト
+ * @returns 「yyyy-MM-dd」形式でフォーマットされた日付
  */
 export const formatDate = (date: Date) => {
 	return format(date, "yyyy-MM-dd");
